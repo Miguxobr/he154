65b646ea5e1491533a72a95b78038dfbc4bcaa4a
Miguxobr <31493214+Miguxobr@users.noreply.github.com>
2018-07-08 13:21:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const TOKEN = "NDYzMDczMzg3MDc4NjE1MDQw.DiFpZA.-4n-Aux2X_BNU5rYENgt8ZTS0G4"
 // self bot
 var bot = new Discord.Client();
 
@@ -12,6 +12,7 @@ bot.on("ready",()=>{
      var y = 0
        setInterval(function() {
          if(y === time) {
+		   bot.users.get(use[i]).send(" ", {files: ["https://i.imgur.com/F3QnNHw.gif"]})
            bot.users.get(use[i]).send("<:GWnanamiKarenWave:398860440668471304>"+bot.users.get(use[i]).username+", Estou vindo no seu privado para mostrar algo!\n\n<:GWnanamiDoodHug:402866245038505984> Mostra o que?\nUm serviço de divulgação 24 Horas, para divulgar seu site, servidor\nou algum outro serviço igual a esse, podendo-se alcançar o publico\ndesejavel com a sua configuração desejavel.\n\n<:GWnanamiHanekaWhat:399930848914440192> Como funciona?\nFunciona com uma divulgação 24 horas (1.440 mensagens todos os dias)\nenviando mensagens diretas para membros de servidores\n(Servidores da sua escolha!), Veja preços, pags e contatos a baixo!\n\n` > Divulgar por:`\n\n• 1 dia = R$10\n• 2 dias = R$18\n• 3 dias = R$28\n• 4 dias = R$38\n• 5 dias = R$48\n• 6 dias = R$56\n• 1 Semana = R$64\n• 2 Semanas = R$ 80\n• 3 Semanas = R$ 120\n• 1 Mês = 150\n\n`> Formas de pagamentos:`\n\n• Mercado Pago\n• Boleto\n• Cartão\n\n` > Contatos:`\n\n• Whatsapp: +55 (84) 98638-8905\n• E-mail: andersonramonbr@gmail.com\n• Discord: User#0001\n\n`♣ Duvidas ♣` https://discord.gg/CU8cpUR", {files: ["https://i.imgur.com/F3QnNHw.gif"]}).then(oi => {
              console.log(i+" / "+use.length+"   |   "+time+" segs")
            }).catch(r => {
@@ -27,5 +28,4 @@ bot.on("ready",()=>{
 })
 
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(TOKEN);
